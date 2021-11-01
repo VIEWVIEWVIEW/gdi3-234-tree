5999596e7e1bb9b8bf31b4f86889cc2ef7987040
VIEWVIEWVIEW <39219018+VIEWVIEWVIEW@users.noreply.github.com>
2021-11-01 18:16:00 +0100

    update main with example

--- gdi3-234tree/main.cpp
+++ gdi3-234tree/main.cpp
@@ -6,7 +6,11 @@
 int main()
 {
     ZdvTree *MemeTree = new ZdvTree();
-    // MemeTree.insertKey(1, 5);
+    MemeTree->insertKey(1, 1);
+    MemeTree->insertKey(5, 5);
+    MemeTree->insertKey(3, 3);
+    MemeTree->insertKey(4, 4);
+
     std::cout << "Hello World!\n";
 }
 
